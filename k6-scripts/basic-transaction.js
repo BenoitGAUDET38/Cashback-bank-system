055a796296d164ee9c3ac2e8c2f640474ff5922c
Benoit Gaudet <bgaudet38@gmail.com>
2023-12-17 17:42:18 +0100

    Change container limitation

--- k6-scripts/basic-transaction.js
+++ k6-scripts/basic-transaction.js
@@ -3,8 +3,8 @@ import { sleep, check } from 'k6';
 
 export let options = {
     stages: [
-        { duration: '1m', target: 1000 }, // Ramp up to 100 users in 1 minute
-        { duration: '3m', target: 1000 }, // Stay at 100 users for 5 minutes
+        { duration: '1m', target: 100 }, // Ramp up to 100 users in 1 minute
+        { duration: '3m', target: 100 }, // Stay at 100 users for 5 minutes
         // { duration: '1m', target: 0 },   // Ramp down to 0 users in 1 minute
     ],
 };
@@ -22,7 +22,7 @@ export function setup() {
 }
 
 export default function (setupData) {
-    let numberOfUsers = 1000;
+    let numberOfUsers = 100;
     let randomUserId = Math.floor(Math.random() * numberOfUsers);
     let user = setupData[randomUserId];
 
